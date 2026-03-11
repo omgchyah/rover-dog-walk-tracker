95ed32c788093eef9a1bc4128dfb51943ac898f6
omgchyah <rossana.liendo@gmail.com>
2026-03-11 19:19:12 +0100

    create models for walk and relations

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Pet(models.Model):
     class PetType(models.TextChoices):
-        DOG = "Dog", "DOG"
-        CAT = "Cat", "CAT"
+        DOG = "Dog"
+        CAT = "Cat"
         
     name = models.CharField(max_length=50, blank=False, null=False)
     #Can be change to owner_id once I create MVP (backlog)
@@ -21,3 +21,40 @@ class Pet(models.Model):
     
     def __str__(self):
         return f"{self.name} ({self.get_pet_type_display()})"
+    
+class Walk(models.Model):
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    
+    pets = models.ManyToManyField(Pet, verbose_name="list of pets", through="PetWalk")
+    
+    def __str__(self):
+        return f"Date: {self.start_time.strftime("%Y-%m-%d %H:%M")}. Pets: {", ".join([p.name for p in self.pets.all()])}"
+
+class PetWalk(models.Model):
+    class MoodTypes(models.TextChoices):
+        AS_USUAL = "As usual"
+        HAPPY = 'Happy'
+        SAD = "Sad"
+        ANXIOUS = "Anxious"
+        LETHARGIC = "Lethargic"
+        TIRED = "Tired"
+        
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    walk = models.ForeignKey(Walk, on_delete=models.CASCADE)
+    poops = models.IntegerField(default=0)
+    pees = models.IntegerField(default=0)
+    mood = models.CharField(
+        max_length=20,
+        choices=MoodTypes,
+        default=MoodTypes.AS_USUAL        
+    )
+
+class WalkPoint(models.Model):
+    walk = models.ForeignKey(Walk, on_delete=models.CASCADE, related_name='points')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['created_at']
